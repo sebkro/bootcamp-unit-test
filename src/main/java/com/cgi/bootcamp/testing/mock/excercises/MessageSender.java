9a01536067b7f515b4bdd0cd0855fbdc4833c57d
Sebastian Krome <sebastian.krome@cgi.com>
2019-10-10 20:48:43 +0200

    uebung 3 - loesung

--- src/main/java/com/cgi/bootcamp/testing/mock/excercises/MessageSender.java
+++ src/main/java/com/cgi/bootcamp/testing/mock/excercises/MessageSender.java
@@ -1,6 +1,11 @@
 package com.cgi.bootcamp.testing.mock.excercises;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MessageSender {
+	
+	private Set<Client> clients = new HashSet<>();
 	
 	/**
 	 * Registriert einen Client
@@ -8,7 +13,8 @@ public class MessageSender {
 	 * @return Anzahl der registrierten clients nach Hinzufuegen des neuen clients
 	 */
 	public int registerClient(Client client) {
-		return 0;
+		clients.add(client);
+		return clients.size();
 	}
 	
 	/**
@@ -16,7 +22,13 @@ public class MessageSender {
 	 * @param message Nachricht die versendet werden soll
 	 */
 	public void sendMessage(Message message) {
-		
+		if (message.getTopic() == null) {
+			clients.forEach(client -> client.sendMessage(message));
+		} else {
+			clients.stream()
+				.filter(c -> c.getInterests().contains(message.getTopic()))
+				.forEach(client -> client.sendMessage(message));
+		}
 	}
 
 }
--- src/test/java/com/cgi/bootcamp/testing/mock/excercises/MessageSenderTest.java
+++ src/test/java/com/cgi/bootcamp/testing/mock/excercises/MessageSenderTest.java
@@ -1,19 +1,29 @@
 package com.cgi.bootcamp.testing.mock.excercises;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class MessageSenderTest {
-	
+
 	private MessageSender sender = new MessageSender();
-	
+
 	private Client client;
-	
+	private Message message;
+
 	@Before
 	public void init() {
+		client = Mockito.mock(Client.class);
+		message = Mockito.mock(Message.class);
 	}
-	
+
 	/**
 	 * - registriere genau einen Client am MessageSender
 	 */
@@ -21,11 +31,9 @@ public class MessageSenderTest {
 	public void itShouldRegisterAClient() {
 		//when
 		int result = sender.registerClient(client);
-		
+
 		//then
 		Assert.assertEquals(1, result);
-				
-				
 	}
 
 	/**
@@ -35,7 +43,15 @@ public class MessageSenderTest {
 	 */
 	@Test
 	public void itShouldSendAMessageToASingleSubscribedClient() {
-		
+		//given
+		sender.registerClient(client);
+
+		//when
+		sender.sendMessage(message);
+
+		//then
+		verify(client).sendMessage(message);
+
 	}
 
 	/**
@@ -45,7 +61,23 @@ public class MessageSenderTest {
 	 */
 	@Test
 	public void itShouldSendAMessageToMultipleSubscribedClients() {
-		
+		//given
+		Client c2 = Mockito.mock(Client.class);
+		Client c3 = Mockito.mock(Client.class);
+		sender.registerClient(client);
+		sender.registerClient(c2);
+		sender.registerClient(c3);
+
+		//when
+		sender.sendMessage(message);
+
+		//then
+		verify(client).sendMessage(message);
+		verify(c2).sendMessage(message);
+		verify(c3).sendMessage(message);
+
+
+
 	}
 
 	/**
@@ -54,7 +86,13 @@ public class MessageSenderTest {
 	 */
 	@Test
 	public void itShouldNotSendMessagesToUnsubscribedClients() {
-		
+		//given dont register client
+
+		//when
+		sender.sendMessage(message);
+
+		//then
+		verify(client, never()).sendMessage(any(Message.class));
 	}
 
 	/**
@@ -63,16 +101,50 @@ public class MessageSenderTest {
 	 */
 	@Test
 	public void itShouldNotSendMessagesTwiceIfClientIsSubscribedTwice() {
-		
+		//given
+		sender.registerClient(client);
+		sender.registerClient(client);
+
+		//when
+		sender.sendMessage(message);
+
+		//then
+		verify(client).sendMessage(message);
+
 	}
-	
+
 	/**
 	 * Eine Message mit Topic soll nur an Clients ausgeliefert werden, die
 	 * sich auf fuer dieses Topic interessieren
 	 */
 	@Test
 	public void itShouldSendAMessageWithTopicOnlyToClientsInterestedInThisTopic() {
-		
+		//given
+		List<String> interestsClient1 = new ArrayList<>();
+		interestsClient1.add("Testen");
+		interestsClient1.add("Java");
+		when(client.getInterests()).thenReturn(interestsClient1);
+
+		List<String> interestsClient2 = new ArrayList<>();
+		interestsClient2.add("Angular");
+		interestsClient2.add("Java");
+		interestsClient2.add("Spring");
+		Client client2 = Mockito.mock(Client.class);
+		when(client2.getInterests()).thenReturn(interestsClient2);
+
+		sender.registerClient(client);
+		sender.registerClient(client2);
+
+		Message message = Mockito.mock(Message.class);
+		when(message.getTopic()).thenReturn("Testen");
+
+		//when
+		sender.sendMessage(message);
+
+		//then
+		verify(client).sendMessage(message);
+		verify(client2, never()).sendMessage(message);
+
 	}
 
 }
